480ba4dbd7d2d2e8904eb3983dd19f3ec0b91cf3
Matt Conser <182449708+mconser858@users.noreply.github.com>
2026-02-27 16:43:13 -0600

    Fixed floating point bug in outputAccountStatus function

--- Final Project (Problem 3)/Final Project (Problem 3).cpp
+++ Final Project (Problem 3)/Final Project (Problem 3).cpp
@@ -18,6 +18,7 @@ int main()
     double credits;
     double creditLimit;
     
+    cout << fixed << setprecision(2);
     for (int i = 0; i < 10; i++)
     {
         inputRetrieval(accountNo, startingBalance, charges, credits, creditLimit);
@@ -49,19 +50,19 @@ double calculateBalance(double startingBalance, double charges, double credits)
 void outputAccountStatus(int accountNo, double currentBalance, double creditLimit)
 {
     cout << "Account #" << accountNo;
-    cout << fixed << setprecision(2);
     if (currentBalance < creditLimit)
     {
         cout << ": $" << creditLimit - currentBalance << " credit available";
     }
-    else if (currentBalance == creditLimit)
-    {
-        cout << " has reached their credit limit.";
-    }
-    else
+    else if (currentBalance > creditLimit)
     {
         cout << " CREDIT LIMIT EXCEEDED\nCredit Limit: $" << creditLimit << "\nCurrent Balance: $" << currentBalance
             << "\nCustomer is overdrafted by $" << currentBalance - creditLimit << ".";
     }
+    else
+    {
+        cout << " has reached their credit limit.";
+    }
+
     cout << endl << endl;
 }
